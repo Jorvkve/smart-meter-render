f0e6417c44e5e74c4ca25defed9dc8cbe675bb55
Jorvkve <Turth00@gmail.com>
2026-02-28 21:10:30 +0700

    fix render port

--- app.py
+++ app.py
@@ -31,5 +31,8 @@ def upload_image():
         "filename": filename
     })
 
+import os
+
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=10000)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
